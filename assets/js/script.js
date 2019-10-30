66f08b3ed250db333a3c872a63cfd84f19a4dae7
Leisa Bryan <lljbryan123@gmail.com>
2019-10-29 17:41:03 -0700

    adding

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,11 +19,13 @@ function getFiveDayWeatherData(city) {
     var weather = response.list
     var count = 0;
     for (var i = 0; i < weather.length; i++) {
+      
       var element = weather[i];
       if (element.dt_txt.includes("18:00:00")) {
         console.log("#weather-" + count, element);
 
         $("#weather-" + count).append(
+          $("<p>").text(element.dt_txt),
           $("<p>").text("Temp: " + element.main.temp),
           $("<p>").text("Humidity: " + element.main.humidity),
           
@@ -37,7 +39,14 @@ function getFiveDayWeatherData(city) {
   // bbb
 }
 
-// q={city name},{US}
+// var imgURL = response.Poster;
+
+// // Creating an element to hold the image
+// var image = $("<img>").attr("src", imgURL);
+
+// // Appending the image
+// movieDiv.append(image);
+
 
 function getWeatherData(city) {
   var baseURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=f9d06868bafd4c49eafce461023e2479&q=" + city;
@@ -51,6 +60,7 @@ function getWeatherData(city) {
     // console.log(response);
     $(".city").html("City: " + response.name);
     // $(".icon").attr(response.weather.icon)
+    console.log(response.weather.icon);
     $(".temperature").text("Temperature:" + response.main.temp);
     $(".humidity").text("Humidity: " + response.main.humidity);
     $(".wind-speed").text("Wind Speed: " + response.main.temp);
